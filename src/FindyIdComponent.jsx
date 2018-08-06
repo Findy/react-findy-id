5d2ba91bb65210b45dfafe7a784ab2fbe7d3b6bf
Matsuda Yuichiro <Yuichiro@Matsuda-MBP.local>
2018-08-07 07:37:49 +0900

    :recycle: remove bind

--- src/FindyIdComponent.jsx
+++ src/FindyIdComponent.jsx
@@ -6,31 +6,16 @@ import { firebase } from '@firebase/app';
 import '@firebase/auth';
 import * as firebaseui from 'firebaseui'
 import 'firebaseui/dist/firebaseui.css';
+import { firebaseConfig } from './config';
 
 export default class FindyIdComponent extends Component {
 
   constructor(props) {
     super(props);
+    firebase.initializeApp(firebaseConfig);
 
-    this.firebaseUiCallbacks = this.firebaseUiCallbacks.bind(this);
-
-    this.firebaseConfig = this.firebaseConfig.bind(this);
-    firebase.initializeApp(this.firebaseConfig());
-
-    this.firebaseUiConfig = this.firebaseUiConfig.bind(this);
     var ui = new firebaseui.auth.AuthUI(firebase.auth());
     ui.start('#firebaseui-auth-container', this.firebaseUiConfig());
-  }
-
-  firebaseConfig() {
-    return ({
-      apiKey: "AIzaSyDOTe8SVN_5WGtuKdXQoQy5slhAq3kEIbs",
-      authDomain: "findy-id.firebaseapp.com",
-      databaseURL: "https://findy-id.firebaseio.com",
-      projectId: "findy-id",
-      storageBucket: "findy-id.appspot.com",
-      messagingSenderId: "943881521886"
-    });
   }
 
   firebaseUiConfig() {
---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDOTe8SVN_5WGtuKdXQoQy5slhAq3kEIbs",
+  authDomain: "findy-id.firebaseapp.com",
+  databaseURL: "https://findy-id.firebaseio.com",
+  projectId: "findy-id",
+  storageBucket: "findy-id.appspot.com",
+  messagingSenderId: "943881521886"
+};
+
