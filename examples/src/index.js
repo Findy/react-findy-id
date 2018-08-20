3008360bb4dc63ff09d761eab8105a42c4174406
Rui Onodera <deraru@gmail.com>
2018-08-20 12:26:43 +0900

    Update example app

--- examples/src/index.js
+++ examples/src/index.js
@@ -4,8 +4,9 @@ import { FindyIdComponent } from '../../src';
 
 const afterSignInSuccessCallback = (authResult) => {
   authResult.user.getIdToken().then(function(idToken) {
-    const message = 'Hello! ' + authResult.additionalUserInfo.username + '\n' +
-      'Your ID Token is below.\n\n' + idToken;
+    const message = 'Hello! ' + authResult.additionalUserInfo.username + '\n\n' +
+      'Your access token on GitHub is below.\n' + authResult.credential.accessToken + '\n\n' +
+      'Your ID Token on Firebase is below.\n' + idToken;
 
     alert(message);
   });
