188c55235f7d362df6a3d1dc4fca384bff6ac42f
keik <keik@c-fo.com>
2019-02-01 00:05:34 +0900

    :+1: Configurable firebase app setting via global environment

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import 'firebase/auth'
 
 import firebaseConfig from './firebaseConfig'
 
-// initialize when loaded
-firebase.initializeApp(firebaseConfig)
+// Initialize when loaded.
+// If `window.__FINDY_FIREBASE_CONFIG__` defined, use one to be convenient for development mainly.
+firebase.initializeApp(window.__FINDY_FIREBASE_CONFIG__ || firebaseConfig)
 
 export * from './FirebaseAuthRepository'
 export { FindyIdComponent } from './FindyIdComponent'
