d34828d4316a9c1d4063a421c1c9f37c834f44ef
keik <keik@c-fo.com>
2019-01-31 22:46:54 +0900

    :shower: Not configurable firebase app by NODE_ENV

--- src/config.js
+++ src/config.js
@@ -1,18 +1,8 @@
-export const firebaseConfig =
-  process.env.NODE_ENV === 'production'
-    ? {
-        apiKey: 'AIzaSyDOTe8SVN_5WGtuKdXQoQy5slhAq3kEIbs',
-        authDomain: 'findy-id.firebaseapp.com',
-        databaseURL: 'https://findy-id.firebaseio.com',
-        projectId: 'findy-id',
-        storageBucket: 'findy-id.appspot.com',
-        messagingSenderId: '943881521886'
-      }
-    : {
-        apiKey: 'AIzaSyCg_rSKtCxZr1W69VNWI4s-lhj53QJ4Fdk',
-        authDomain: 'findy-id-development.firebaseapp.com',
-        databaseURL: 'https://findy-id-development.firebaseio.com',
-        projectId: 'findy-id-development',
-        storageBucket: 'findy-id-development.appspot.com',
-        messagingSenderId: '614464328426'
-      }
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDOTe8SVN_5WGtuKdXQoQy5slhAq3kEIbs',
+  authDomain: 'findy-id.firebaseapp.com',
+  databaseURL: 'https://findy-id.firebaseio.com',
+  projectId: 'findy-id',
+  storageBucket: 'findy-id.appspot.com',
+  messagingSenderId: '943881521886'
+}
